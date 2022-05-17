13ca3d3c5c42facb36a2f77a9c374572193fb5f9
jcbl1 <x@rincons.cc>
2022-05-17 16:51:44 +0800

    fixed file existence bug

--- fileOpers/fileOpers.go
+++ fileOpers/fileOpers.go
@@ -1,21 +1,25 @@
 package fileOpers
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 )
 
 func AddXP(a int) {
 	homePath := os.Getenv("HOME")
-	f, err := os.Open(homePath + "/.myKegelTimer/xp.dat")
+	err:=os.Mkdir(homePath+"/.myKegelTimer",0755)
+	if err!=nil&&!os.IsExist(err){
+		panic(err.Error())
+	}
+	f, err := os.OpenFile(homePath+"/.myKegelTimer/xp.dat", os.O_CREATE|os.O_RDONLY, 0644)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	rdr:=bufio.NewReader(f)
+	rdr := bufio.NewReader(f)
 
-	line,_,_:=rdr.ReadLine()
+	line, _, _ := rdr.ReadLine()
 
 	fmt.Println(a)
 	fmt.Println(string(line))
